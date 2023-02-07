c115b14620c51e8f736ce1bbcb5feb6271a9cd3d
ikkos <106231087+ikkos@users.noreply.github.com>
2023-02-07 20:01:59 +1100

    s

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById("reveal-button").addEventListener("click", function(){
     document.getElementById("rose").classList.remove("hidden");
+    document.getElementById("text").classList.remove("hidden");
+    document.getElementById("arrow").classList.remove("hidden");
+    document.getElementById("flower").classList.remove("hidden");
   });
